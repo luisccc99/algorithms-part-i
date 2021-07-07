6caeb510cae5d3d7cb6683e4856abc62fbd3f0d9
Luis Cruz <luisccc99@gmail.com>
2021-07-07 14:47:34 -0600

    Add programs to test library's book

---
+++ Hello/RandomWord.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+	public static void main(String[] args) {
+		String champion = StdIn.readString();
+		int i = 1;
+		while (!StdIn.isEmpty()) {
+			String current = StdIn.readString();
+			double p = 1.0 / ++i;
+			boolean newChampion = StdRandom.bernoulli(p);
+			if (newChampion) {
+				champion = current;
+			}
+		}
+		StdOut.println(champion);
+	}
+}
