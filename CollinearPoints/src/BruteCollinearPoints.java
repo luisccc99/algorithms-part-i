ceacc42b50a7fc84782b5a571782500e4214e926
Luis Cruz <luisccc99@gmail.com>
2021-07-21 21:19:28 -0600

    Add code snippets of third week's lecture and programming assingment 'CollinearPoint'

---
+++ CollinearPoints/src/BruteCollinearPoints.java
@@ -0,0 +1,24 @@
+public class BruteCollinearPoints {
+
+    /**
+     * finds all line segments containing 4 points
+      * @param points array of points to look through
+     */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * @return the number of line segments
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /**
+     * @return the line segments
+     */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
